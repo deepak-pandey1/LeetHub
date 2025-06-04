4eed91b5779d37ace887f4f4284abc5c0d75d307
Deepak Pandey <deepakpandeyofficially@gmail.com>
2025-06-04 15:14:17 +0530

    Time: 0 ms (100%), Space: 35.4 MB (92.84%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    TreeNode* search(TreeNode*&root, int val){
+    TreeNode* search(TreeNode* root, int val){
         if(root == NULL) return NULL;
 
         if(root->val == val) return root;
