a2f8a05e7086324a6266653197ccc320f496e7a5
Deepak Pandey <deepakpandeyofficially@gmail.com>
2025-06-14 18:55:59 +0530

    Time: 0 ms (100%), Space: 64.8 MB (91.55%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -5,13 +5,14 @@ public:
     void rangeSum(TreeNode* root, int low, int high) {
         if (root == NULL) return;
 
+        // If the current node is in the range, add its value
+        if (root->val >= low && root->val <= high)
+            sum += root->val;
+            
         // Only go left if there's a chance smaller values are in range
         if (root->val > low)
             rangeSum(root->left, low, high);
 
-        // If the current node is in the range, add its value
-        if (root->val >= low && root->val <= high)
-            sum += root->val;
 
         // Only go right if there's a chance larger values are in range
         if (root->val < high)
