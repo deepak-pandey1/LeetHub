c92f49dde112361bf957f0f6e60742418a9b48af
Deepak Pandey <deepakpandeyofficially@gmail.com>
2025-06-15 20:22:47 +0530

    Time: 0 ms (100%), Space: 16 MB (97.07%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -7,8 +7,6 @@ public:
         if(root->left == NULL && root->right == NULL)
             return true;
 
-        checkLeaves(root->left);
-        checkLeaves(root->right);
         return false;
     }
     int sumOfLeftLeaves(TreeNode* root) {
