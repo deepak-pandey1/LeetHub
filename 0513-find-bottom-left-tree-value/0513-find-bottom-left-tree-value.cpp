792a4ef7f6a0ba30fbd396175a4ed40009401b83
Deepak Pandey <deepakpandeyofficially@gmail.com>
2025-06-21 13:37:52 +0530

    Time: 0 ms (100%), Space: 25 MB (63.71%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<int> leftMost;
-    void leftTraversal(TreeNode* root, int level){
-        if(root == NULL) return;
-
-        if(level == leftMost.size()) leftMost.push_back(root->val);
-
-        leftTraversal(root->left, level+1);
-        leftTraversal(root->right, level+1);
-    }
     int findBottomLeftValue(TreeNode* root) {
-        if(root == NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode* front = NULL;
 
-        leftTraversal(root, 0);
-        return leftMost[leftMost.size()-1];
+        while(!q.empty()){
+            front = q.front();
+            q.pop();
+
+            if(front->right != NULL) q.push(front->right);
+            if(front->left != NULL) q.push(front->left);
+        }
+
+        return front->val;
     }
 };
