60ab3997337c82a3cbc89a4ab59d143a4e73eefd
Deepak Pandey <deepakpandeyofficially@gmail.com>
2025-08-07 09:20:57 +0530

    Time: 10 ms (37.43%), Space: 21 MB (60.93%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj;
+    vector<string> itinerary;
+
+    void dfs(string src) {
+        while (!adj[src].empty()) {
+            string next = adj[src].top();
+            adj[src].pop();
+            dfs(next);
+        }
+        itinerary.push_back(src);
+    }
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for (auto& ticket : tickets) {
+            adj[ticket[0]].push(ticket[1]);
+        }
+
+        dfs("JFK");
+
+        reverse(itinerary.begin(), itinerary.end());
+        return itinerary;
+    }
+};
